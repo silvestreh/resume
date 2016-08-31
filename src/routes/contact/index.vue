e49a90329ef70c7c7c262a6ac4e825dba0c6fb46
Silvestre Herrera <silvestre.herrera@gmail.com>
2016-08-31 18:45:08 -0300

    Optimized images

---
+++ src/routes/contact/index.vue
@@ -0,0 +1,3 @@
+<template src="./contact-template.html"></template>
+<script src="./contact-controller.js"></script>
+<style src="./contact-styles.scss" lang="scss"></style>
--- src/routes/routes-map.js
+++ src/routes/routes-map.js
@@ -1,4 +1,5 @@
 import about from 'routes/about';
+import contact from 'routes/contact';
 import home from 'routes/home';
 import notFound from 'routes/not-found';
 import resume from 'routes/resume';
@@ -12,6 +13,9 @@ export default {
     },
     '/about': {
         component: about,
+    },
+    '/contact': {
+        component: contact,
     },
     '/resume': {
         component: resume,
