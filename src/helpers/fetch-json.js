689d603d7976b7aae3d829e172f81e84a1f75525
Silvestre Herrera <silvestre.herrera@gmail.com>
2016-09-01 20:31:40 -0300

    Send lcov report to Codacy

--- src/helpers/fetch-json.js
+++ src/helpers/fetch-json.js
@@ -1,3 +1,11 @@
+/**
+ * Fetches JSON data and stores it in a VM instance
+ *
+ * @param   {Object} vm Vue instance
+ * @param   {String} path The JSON filename
+ * @return  {Promise} Promise
+ */
+
 export default function (vm, path) {
     vm.$http.get(`/static/json/${path}.json`)
         .then((response) => {
