f8652173785b81cb3a9294f3ccfb6c4930899191
Silvestre Herrera <silvestre.herrera@gmail.com>
2016-08-31 23:13:19 -0300

    Fake XMLHttpRequests

--- test/unit/specs/routes/resume.spec.js
+++ test/unit/specs/routes/resume.spec.js
@@ -1,26 +1,81 @@
 import Vue from 'vue';
 import VueResource from 'vue-resource';
 import resume from 'routes/resume';
+import fetchJSON from 'src/helpers/fetch-json';
+
+// Static data
+import awards from '../../../../static/json/awards.json';
+import education from '../../../../static/json/education.json';
+import experience from '../../../../static/json/experience.json';
+import recommendations from '../../../../static/json/recommendations.json';
 
 Vue.use(VueResource);
 
 describe('Resume route', () => {
     it('should have default data', () => {
         const data = resume.data();
-
         expect(typeof data).to.equal('object');
     });
 
     it('should fetch JSON data', (done) => {
         Vue.http.interceptors.push((request, next) => {
-            const fakeData = {
-                data: [
-                    'Fake',
-                ],
-            };
-            next(request.respondWith(fakeData, {
-                status: 200,
-            }));
+            let res;
+
+            switch (request.url) {
+            case '/static/json/awards.json': {
+                res = {
+                    status: 200,
+                    statusText: 'Ok',
+                    ok: true,
+                    data: awards,
+                };
+                break;
+            }
+            case '/static/json/education.json': {
+                res = {
+                    status: 200,
+                    statusText: 'Ok',
+                    ok: true,
+                    data: education,
+                };
+                break;
+            }
+            case '/static/json/experience.json': {
+                res = {
+                    status: 200,
+                    statusText: 'Ok',
+                    ok: true,
+                    data: experience,
+                };
+                break;
+            }
+            case '/static/json/recommendations.json': {
+                res = {
+                    status: 200,
+                    statusText: 'Ok',
+                    ok: true,
+                    data: recommendations,
+                };
+                break;
+            }
+            default: {
+                res = {
+                    status: 404,
+                    statusText: 'Not found',
+                    ok: false,
+                    data: 'NOT FOUND',
+                };
+                break;
+            }
+            }
+
+            next((response) => {
+                response.status = res.status;
+                response.statusText = res.statusText;
+                response.ok = res.ok;
+                response.data = res.data;
+                return response;
+            });
         });
 
         const vm = new Vue({
@@ -35,14 +90,18 @@ describe('Resume route', () => {
 
         const instance = vm.$refs.resumeSection;
 
+        // Fetch all data through the READY hook
         resume.ready.apply(instance);
 
-        vm.$nextTick(() => {
-            expect(instance.awards.length).to.equal(0);
-            expect(instance.education.length).to.equal(0);
-            expect(instance.experience.length).to.equal(0);
-            expect(instance.recommendations.length).to.equal(0);
+        // Try fetching something that doesn't exist
+        fetchJSON(instance, 'doesNotExist');
+
+        setTimeout(() => {
+            expect(instance.awards.length).to.equal(1);
+            expect(instance.education.length).to.equal(2);
+            expect(instance.experience.length).to.equal(4);
+            expect(instance.recommendations.length).to.equal(4);
             done();
-        });
+        }, 1000);
     });
 });
